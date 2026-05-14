502f62c4933f953348059f8b497bdda0a7ce3d20
Andrés Campos Montero <a.campos.5@estudiantec.cr>
2026-05-14 17:49:36 -0600

    Estructura del proyecto y CU1

---
+++ caso2-reservar-cita/ReservarCita.jsx
@@ -0,0 +1,11 @@
+function ReservarCita() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Reservar Cita
+      </h1>
+    </div>
+  );
+}
+
+export default ReservarCita;
